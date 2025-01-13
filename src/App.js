101e0f404a05225be4e42abf17bdc5221613de77
samadhan-Kokare <165460307+samadhan-Kokare@users.noreply.github.com>
2025-01-13 16:43:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setCount(count + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  });
+
+  let handleStart = () => {
+    setIsRunning(true);
+  };
+
+  let handleStop = () => {
+    setIsRunning(false);
+  };
+
+  let handleReset = () => {
+    setIsRunning(false);
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p id="counters">
+        Counter is <span>{count}</span>
+      </p>
+      <button id="start" onClick={handleStart}>
+        start
+      </button>
+      <br />
+      <br />
+      <button id="stop" onClick={handleStop}>
+        stop
+      </button>
+      <br />
+      <br />
+      <button id="reset" onClick={handleReset}>
+        Reset
+      </button>
+    </div>
+  );
+}
