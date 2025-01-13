3579af809aa0d9fe07f8a24165e722b21cf4ca09
Samadhan Kokare <165460307+samadhan-Kokare@users.noreply.github.com>
2025-01-13 11:43:24 +0000

    setCount issue

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ export default function App() {
 
     if (isRunning) {
       timer = setInterval(() => {
-        setCount(count + 1);
+        setCount((prevCount) => prevCount + 1);
       }, 1000);
     } else {
       clearInterval(timer);
     }
 
-    return () => clearInterval(timer);
-  });
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isRunning]);
 
   let handleStart = () => {
     setIsRunning(true);
